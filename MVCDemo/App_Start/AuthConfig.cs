d92ee77ff6120d0ca723c97fc2d9db0bc58f62d9
hanse174 <hanse174@JB202-L20-W-D>
2019-04-29 20:46:03 -0400

    Finished L9

---
+++ MVCDemo/App_Start/AuthConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetOpenAuth.GoogleOAuth2;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+namespace MVCDemo.App_Start
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            GoogleOAuth2Client clientGoog = new GoogleOAuth2Client("628865352789-oomk71j3u8url8u7odav2jh6ogrgo88m.apps.googleusercontent.com", "f7FucqlP0BKch8NmbCVoX1Ky");
+        IDictionary<string, string> extraData = new Dictionary<string, string>();
+            OpenAuth.AuthenticationClients.Add("google", () => clientGoog, extraData);
+        }
+    }
+}
